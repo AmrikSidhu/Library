780cf88c8eb3a91f575272ee3d2dda30c64bdf1a
zangrazyjatt <eramriksidhu@gmail.com>
2019-09-14 17:57:52 -0400

    TC1-Red AdminLoggedin.

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,4 +1,22 @@
 
 public class LibraryApp {
+	public boolean adminLoggedIn()
+	{
+		return true;
+	}
+	public boolean adminLogin( String password)
+	{
+		if(password.equals("password"))
+		{
+			return true;
+			
+		} 
+		else
+		{
+			return false;
+			
+		}
+	
+	}
 
 }
